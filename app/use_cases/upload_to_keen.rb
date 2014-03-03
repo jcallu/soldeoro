8ac2d03fd2acba5bb3e93a033579f5bb378304fa
Jacinto Callu <jacintocallu@gmail.com>
2014-03-03 03:08:35 -0800

    compiling_more

--- app/use_cases/upload_to_keen.rb
+++
@@ -1,8 +0,0 @@
-class UploadToKeen
-  include PTT.metrics.defaults
-
-  def perform(attrs)
-    event_type = attrs.delete('event_type')
-    Keen.publish(event_type, attrs) if AppEnv[:keen_project_id].to_s.present?
-  end
-end
