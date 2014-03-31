3e98e430950eb049150134a0490aea67c7ffb51e
Jacinto Callu <jacintocallu@gmail.com>
2014-03-31 16:33:02 -0700

    Add logo and content with to new layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,7 @@
 module ApplicationHelper
-
-  def arrow_for_comparison(starting, ending)
-    arrow_percentage((ending - starting).to_f / starting)
-  end
-
-  def arrow_percentage(percentage)
-    direction = percentage > 0 ? 'up' : 'down'
-    content_tag(:div, '', class: "icon flaticon stroke arrow-#{direction}-2") +
-    number_to_percentage(percentage.abs * 100, precision: 0)
-  end
-
-  def large_currency(number)
-    number_to_currency(number/1000, precision: 1) + 'k'
+  def phone_number_link(text)
+    sets_of_numbers = text.scan(/[0-9]+/)
+    number = "#{sets_of_numbers.join('-')}"
+    link_to text, "tel:#{number}"
   end
 end
